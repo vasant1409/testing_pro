474261b73e0ff57fefd512c272c7ce708d22db1e
vasant1409 <149039305+vasant1409@users.noreply.github.com>
2023-10-27 18:38:34 +0530

    Update marrt_ma.sql

--- models/example/marrt_ma.sql
+++ models/example/marrt_ma.sql
@@ -2,9 +2,9 @@ WITH
    {{config(materialized='table')}}
 
 MART as (
-SELECT * FROM {{ref(`sample1_stg1`)}} 
+SELECT * FROM {{ref('sample1_stg1')}} 
 UNION ALL
-SELECT * FROM {{ref(`sample2_stg2`)}} 
+SELECT * FROM {{ref('sample2_stg2')}} 
 ) 
 
 SELECT * FROM MART
